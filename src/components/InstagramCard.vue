115907927b98624a4a7267301120770950798ac7
Your Name <you@example.com>
2019-12-12 20:40:37 +0100

    first commit

---
+++ src/components/InstagramCard.vue
@@ -0,0 +1,49 @@
+<template>
+
+      <div class="card text-center">
+        <div class="card-header text-left">
+          {{ info.title }}
+        </div>
+        <div class="card-body">
+          <img @click="change" class="card-img-top" :src="info.url" alt="Maznusmo s unsplasha i bilo bi lijepo da napisemo cija je slika">
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-6">
+            <div class="card-footer text-left">
+             {{ info.time }}
+            </div>
+            </div>
+            <div class="col-6">
+            <div class="card-footer text-right">
+               {{info.user}}
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+</template>
+
+<script>
+export default {
+  props: ["info"],
+
+  methods: {
+    change() {
+      this.info.time = 'Fetching...'
+      fetch(`https://source.unsplash.com/1600x900`).then((response) => {   
+        console.log(response)
+        this.info.url = response.url
+        this.info.time = 'Done.'
+      }) 
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  img:hover {
+    cursor: pointer;
+  }
+</style>
---
+++ src/store.js
@@ -0,0 +1,10 @@
+export default {
+    authenticated: false,
+    userEmail: 'fake@email.com',
+    userName: 'Neki user',
+    searchTerm: '',
+    cards: [
+      { user: 'marko', title: 'jedan', time: 'an hour ago', url: '/images/unsplash.jpg' },
+      { user: 'ivana', title: 'dva', time: '2 days ago', url: '/images/unsplash.jpg' },
+    ]        
+}
